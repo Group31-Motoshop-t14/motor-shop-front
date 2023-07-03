0f70cfa336e677025ebb72e34d4c9180368600ac
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-07-03 09:50:21 -0300

    fix

--- src/Components/PagesComponents/Announcements/AnnouncerCard/index.tsx
+++ src/Components/PagesComponents/Announcements/AnnouncerCard/index.tsx
@@ -30,8 +30,8 @@ const AnnouncerCard = ({ user }: AnnouncerCardProps) => {
           <a
             target="_blank"
             href={`https://api.whatsapp.com/send?phone=+55+${1234567899}&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conhecer%20melhor%20seus%20servi%C3%A7os`}
-            className="prose-body-2-500-400 text-grey1">
-            +5599197859
+            className="prose-body-2-500 text-grey1 hover:text-Brand2">
+            Contacta o anunciante
           </a>
         </div>
       </div>
