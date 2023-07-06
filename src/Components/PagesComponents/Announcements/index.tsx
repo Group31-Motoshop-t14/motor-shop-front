445d010d0dda6840c9bc9ea523f2d7cca8c2146a
rafaeuus <rafaeljcarvalhosilva@gmail.com>
2023-07-06 11:13:34 -0300

    fix: fixing buy button

--- src/Components/PagesComponents/Announcements/index.tsx
+++ src/Components/PagesComponents/Announcements/index.tsx
@@ -93,7 +93,15 @@ export const AnnouncementsMain = ({ carsAnnouncement, carComments }: Iannounceme
                   </span>
                 </div>
                 {userAuth && (
-                  <Button variant={"gradient"} color={"blue"} size={"secondary"}>
+                  <Button
+                    variant={"gradient"}
+                    color={"blue"}
+                    size={"secondary"}
+                    onClick={() =>
+                      (window.location.href = `https://api.whatsapp.com/send?phone=+${
+                        userProfile!.phone
+                      }&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conhecer%20melhor%20seus%20servi%C3%A7os`)
+                    }>
                     Comprar
                   </Button>
                 )}
