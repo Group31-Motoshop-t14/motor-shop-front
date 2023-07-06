561c53078921217c25f9b1b62f5a32ed79b61ca1
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-07-05 22:42:20 -0300

    fix card, add LINK

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { AuthContext } from "@/contexts/AuthContext";
-import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useContext } from "react";
 
 export interface IcarAnnouncement {
@@ -38,7 +39,6 @@ interface CardProps {
 const Card = ({ car }: CardProps) => {
   const { userAuth } = useContext(AuthContext);
 
-  const router = useRouter();
   const pathName = usePathname();
 
   if (car.coverImage === "") {
@@ -50,57 +50,58 @@ const Card = ({ car }: CardProps) => {
   }
 
   return (
-    <div
-      className="group m-2 flex h-[342px] w-full min-w-[280px] max-w-xs flex-col justify-between hover:cursor-pointer"
-      onClick={() => router.push(`/announcement/${car.id}`)}>
-      <div className="relative flex h-36 max-h-36 w-full justify-center border-2 border-grey7 bg-grey7 object-cover transition  group-hover:border-Brand1">
-        <picture className="h-[140px] w-full">
-          <img
-            className="h-full w-full  bg-transparent object-contain"
-            src={car.coverImage}
-            alt="Imagem do veículo"
-            width={312}
-            height={132}
-          />
-        </picture>
-        {isAdvertiserOwner && pathName !== "/" && (
-          <span
-            className={`prose-body-2-600 absolute left-4 top-[11px] bg-Brand1 px-2 text-grey10 ${
-              car.isPublished ? "bg-Bran1" : "bg-grey4"
-            }`}>
-            {car.isPublished ? "Ativo" : "Inativo"}
-          </span>
-        )}
-        {(car.price <= car.fipePrice || (5 / 100) * car.fipePrice >= car.price - car.fipePrice) && (
-          <span className="prose-body-2-600 absolute right-0 top-0 bg-Green1 px-1 py-1 text-grey10">
-            $
-          </span>
-        )}
-      </div>
-      <div className="flex h-[182px] flex-col justify-between text-grey1">
-        <p className="prose-heading-7-600 truncate">{car.model}</p>
-        <p className="prose-body-2-400 text-grey2 line-clamp-2 ">{car.description}</p>
-        <div className="flex items-center gap-2">
-          <span className="prose-body-2-600 flex h-8 w-8 items-center justify-center rounded-full bg-Brand2 text-grey10">
-            {car.user.name[0].toUpperCase()}
-          </span>
-          <span className="prose-body-2-600 text-grey2">{car.user.name}</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex gap-3">
-            <span className="prose-body-2-600  rounded bg-Brand4 px-2 py-1 text-Brand1">
-              {car.mileage} KM
+    <Link href={`/announcement/${car.id}`}>
+      <div className="group m-2 flex h-[342px] w-full min-w-[280px] max-w-xs flex-col justify-between hover:cursor-pointer">
+        <div className="relative flex h-36 max-h-36 w-full justify-center border-2 border-grey7 bg-grey7 object-cover transition  group-hover:border-Brand1">
+          <picture className="h-[140px] w-full">
+            <img
+              className="h-full w-full  bg-transparent object-contain"
+              src={car.coverImage}
+              alt="Imagem do veículo"
+              width={312}
+              height={132}
+            />
+          </picture>
+          {isAdvertiserOwner && pathName !== "/" && (
+            <span
+              className={`prose-body-2-600 absolute left-4 top-[11px] bg-Brand1 px-2 text-grey10 ${
+                car.isPublished ? "bg-Bran1" : "bg-grey4"
+              }`}>
+              {car.isPublished ? "Ativo" : "Inativo"}
             </span>
-            <span className="prose-body-2-600  rounded bg-Brand4 px-2 py-1 text-Brand1">
-              {car.year}
+          )}
+          {(car.price <= car.fipePrice ||
+            (5 / 100) * car.fipePrice >= car.price - car.fipePrice) && (
+            <span className="prose-body-2-600 absolute right-0 top-0 bg-Green1 px-1 py-1 text-grey10">
+              $
+            </span>
+          )}
+        </div>
+        <div className="flex h-[182px] flex-col justify-between text-grey1">
+          <p className="prose-heading-7-600 truncate">{car.model}</p>
+          <p className="prose-body-2-400 text-grey2 line-clamp-2 ">{car.description}</p>
+          <div className="flex items-center gap-2">
+            <span className="prose-body-2-600 flex h-8 w-8 items-center justify-center rounded-full bg-Brand2 text-grey10">
+              {car.user.name[0].toUpperCase()}
+            </span>
+            <span className="prose-body-2-600 text-grey2">{car.user.name}</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex gap-3">
+              <span className="prose-body-2-600  rounded bg-Brand4 px-2 py-1 text-Brand1">
+                {car.mileage} KM
+              </span>
+              <span className="prose-body-2-600  rounded bg-Brand4 px-2 py-1 text-Brand1">
+                {car.year}
+              </span>
+            </div>
+            <span className="prose-heading-7-500">
+              {car.price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
             </span>
           </div>
-          <span className="prose-heading-7-500">
-            {car.price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
-          </span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
